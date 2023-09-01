7c8996a43fe5d4ebd72950ae9373e33db0b926ca
denliedesign <customdenlie@gmail.com>
2023-09-01 10:10:33 -0500

    calendar images

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -7,7 +7,9 @@
 
     <div class="container">
         <h1 class="text-center my-4">Calendar</h1>
-        <p class="text-center">Check back soon for our calendar and upcoming events!</p>
+{{--        <p class="text-center">Check back soon for our calendar and upcoming events!</p>--}}
+        <img src="/images/23-calendar-2.jpg" alt="calendar" class="img-fluid">
+        <img src="/images/23-calendar-1.jpg" alt="calendar" class="img-fluid">
     </div>
 
 @endsection
